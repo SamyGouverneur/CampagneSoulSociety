6284430f9c8a33f9cd431d2079bb99f584924422
Helene Bloch <ln-45@live.fr>
2019-04-01 14:57:16 +0200

    adding navbar with links and logo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap';
 import './App.scss';
+import { BrowserRouter } from "react-router-dom";
+import Navbar from './Components/Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-      </div>
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+                </header>
+                <Navbar/>
+            </div>
+        </BrowserRouter>
     );
   }
 }
