930f51692d5c5e09dea9abf70b4c6d8fb62c8ebf
Helene Bloch <ln-45@live.fr>
2019-04-02 22:43:44 +0200

    delete line navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,6 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                         aria-label="Toggle navigation">
-
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
