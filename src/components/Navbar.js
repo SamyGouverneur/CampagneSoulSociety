ff1201cee8967a365bf1afbb7d1dae7bab86ee33
Helene Bloch <ln-45@live.fr>
2019-04-02 11:57:07 +0200

    navbar fixed top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Navbar = () => {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light">
                 <img alt="Logo" src="../../images/defaultLogo.png" className="logo ml-5"/>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                         aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div id="navbar" className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <ul className="navbar-nav right">
                         <a className="nav-item nav-link my-2 ml-5 mr-5" href="#home">Le concept</a>
                         <a className="nav-item nav-link my-2 ml-5 mr-5" href="#">Tarifs</a>
