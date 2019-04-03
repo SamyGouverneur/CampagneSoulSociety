371a913e07f2e212ad818b7bd59c4d9c4797739d
Helene Bloch <ln-45@live.fr>
2019-04-03 17:17:42 +0200

    adding concept section

--- src/components/concept/Concept.js
+++ src/components/concept/Concept.js
@@ -1,27 +1,20 @@
 import React from 'react'
-import Step from './Step.js';
 
 const Concept = () => {
     return (
-        <div id="concept" className="row">
-            <div className="col-xs-12 my-auto">
-                <h2>Le Concept</h2>
-                <p>Il vous suffit de manger 5 fruits et légumes.</p>
-                <div className="steps">
-                    <div className="row">
-                        <Step img="/images/penguin.png"
-                            number={1} title={'Je choisis mon format'}
-                            description={'Eo adducta re per Isauriam, rege Persarum bellis finitimis inligato repellenteque a conlimitiis suis ferocissimas gentes, quae mente quadam versabili hostiliter eum saepe incessunt et in nos arma moventem aliquotiens iuvant, Nohodares quidam nomine e numero Atchoum.'} />
+        <div id="concept" className="container-fluid">
+            <div className="container my-auto">
+                <div className="row justify-content-center concept">
+                    <div className="col-lg-6 col-md-12 col-sm-12 my-auto mx-auto text-center">
+                        <img alt="plaque" src="../../images/plaque_ombre.png" className="plaque"/>
                     </div>
-                    <div className="row">
-                        <Step img="/images/penguin.png"
-                            number={2} title={'Les participants s\'inscrivent'}
-                            description={'Eo adducta re per Isauriam, rege Persarum bellis finitimis inligato repellenteque a conlimitiis suis ferocissimas gentes, quae mente quadam versabili hostiliter eum saepe incessunt et in nos arma moventem aliquotiens iuvant, Nohodares quidam nomine e numero Atchoum.'} />
-                    </div>
-                    <div className="row">
-                        <Step img="/images/penguin.png"
-                            number={3} title={'Le jour J, je lance l\'appli'}
-                            description={'Eo adducta re per Isauriam, rege Persarum bellis finitimis inligato repellenteque a conlimitiis suis ferocissimas gentes, quae mente quadam versabili hostiliter eum saepe incessunt et in nos arma moventem aliquotiens iuvant, Nohodares quidam nomine e numero Atchoum.'} />
+                    <div className="col-lg-6 col-md-12 col-sm-12 concept_text my-auto">
+                        <h2>Restez Joignable, où que vous soyez...</h2>
+                        <p>Bonjour+ est un interphone connecté nouvelle génération</p>
+                        <p>Le plus fin au monde, le meilleur rapport qualité/prix et plein de fonctionnalités innovantes</p>
+                        <p>Facile à utiliser, vous n'avez qu'à le coller sur votre porte ou boîte aux lettres, et
+                            télécharger l'application.</p>
+                        <p>C'est tout !</p>
                     </div>
                 </div>
             </div>
