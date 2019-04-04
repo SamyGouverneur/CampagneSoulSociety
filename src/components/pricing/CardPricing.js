d98d6dcddacdc236b69f67f8e20871391d955826
Helene Bloch <ln-45@live.fr>
2019-04-04 15:34:19 +0200

    adding pricing section

--- src/components/pricing/CardPricing.js
+++ src/components/pricing/CardPricing.js
@@ -3,14 +3,16 @@ import React from 'react';
 const CardPricing = (props) => {
     return (
         <div className="col-xl-4 col-lg-6 col-md-12 col-sm-12 section_pricing">
-            <div className="card pricing_card mt-5">
-                <div className="card-header mt-4">
-                    <img className="img-responsive picture_pricing" src="/images/penguin.png" alt="picture"/>
-                </div>
+            <div className="card pricing_card mt-3">
                 <div className="card-body">
-                    <h5 className="card-title mt-3 text-center">{props.title}</h5>
-                    <p className="card-text mt-5 text-center">{props.details}</p>
-                    <p className="card-text mt-5 text-center pricing">{props.pricing} €</p>
+                    <h3 className="format text-center mt-3">{props.format}</h3>
+                    <p className="pricing mt-5 text-center">{props.pricing}</p>
+                    <p className="subscription text-center">{props.subscription}</p>
+                    <div className="fonction">
+                        <p className="presentation mt-5">{props.user}</p>
+                        <p className="presentation">{props.equipment}</p>
+                        <p className="presentation">{props.details}</p>
+                    </div>
                     <OrderPricing/>
                 </div>
             </div>
@@ -20,8 +22,8 @@ const CardPricing = (props) => {
 
 const OrderPricing = () => {
     return (
-        <div className="text-center mt-5">
-            <a href="#" className="button mt-5">Commander</a>
+        <div className="plus text-center mt-5">
+            <a href="#contact" className="mt-5">+</a>
         </div>
     );
 }
